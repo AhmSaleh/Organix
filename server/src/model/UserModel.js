1bf027f5bc5550a0a3c2ccdab2860c48d90e5412
AhmSaleh <ahmedsaleh166@gmail.com>
2022-04-21 14:19:53 +0200

    add product model

--- server/src/model/UserModel.js
+++ server/src/model/UserModel.js
@@ -1,56 +1,51 @@
-var mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+var mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-var DB_URL = "mongodb://localhost:27017/Users"
+var DB_URL = "mongodb://localhost:27017/Users";
 
-mongoose.connect(DB_URL, {useNewUrlParser:true});
-
+mongoose.connect(DB_URL, { useNewUrlParser: true });
 
 var UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        match: [/\S+@\S+\.\S+/, 'is invalid'],
-        index: true,
-        required: true,
-        unique: true
-    },
-    hash: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        minlength: [2, 'is too short (minimum is 2 characters)'],
-        required: true
-    }
-    
+  email: {
+    type: String,
+    match: [/\S+@\S+\.\S+/, "is invalid"],
+    index: true,
+    required: true,
+    unique: true,
+  },
+  hash: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    minlength: [2, "is too short (minimum is 2 characters)"],
+    required: true,
+  },
 });
 
-UserSchema.methods.comparePassword = async function(password, hash) {
-    return await bcrypt.compare(password, hash);
+UserSchema.methods.comparePassword = async function (password, hash) {
+  return await bcrypt.compare(password, hash);
 };
 
-UserSchema.methods.createUser = async function(user) {
-    const hash = await bcrypt.hash(user.password, saltRounds);
-    return await mongoose.model('User').create({
-        email: user.email,
-        hash: hash,
-        name: user.name
-    });
+UserSchema.methods.createUser = async function (user) {
+  const hash = await bcrypt.hash(user.password, saltRounds);
+  return await mongoose.model("User").create({
+    email: user.email,
+    hash: hash,
+    name: user.name,
+  });
 };
 
-UserSchema.methods.getUserByEmail = async function(email) {
-    return await mongoose.model('User').findOne({
-        email: email
-    });
+UserSchema.methods.getUserByEmail = async function (email) {
+  return await mongoose.model("User").findOne({
+    email: email,
+  });
 };
 
-UserSchema.methods.getAllUsers = async function() {
-    return await mongoose.model('User').find({});
+UserSchema.methods.getAllUsers = async function () {
+  return await mongoose.model("User").find({});
 };
 
-
-
-
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
