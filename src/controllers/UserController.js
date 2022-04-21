59aa7e5ee91d84caf9f06182fb02e04a8b8b2927
AhmSaleh <ahmedsaleh166@gmail.com>
2022-04-21 19:11:10 +0200

    add Product Contoller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,69 +1,71 @@
-const {ajv} = require("../Utils/validate");
+const { ajv } = require("../Utils/validate");
 const jwt = require("jsonwebtoken");
 
 const UserModel = require("../Model/UserModel");
 
-async function postLogin(req,res){
-    const user = req.body;
-    
-    const validate = ajv.getSchema("userLogin");
-    const valid = validate(user);
-    // check the validation
-    if(!valid) return res.status(400).send(validate.errors);
-    // check the user exist
-    const userModel = new UserModel();
-    const userExist = await userModel.getUserByEmail(user.email);
-    if(!userExist) return res.status(401).send("failed to login");
-    // check the password
-    const isPasswordCorrect = await userModel.comparePassword(user.password,userExist.hash);
-    if(!isPasswordCorrect) return res.status(401).send("failed to login");
-    
-    tokenPayload = {
-        UserId:user.email,
-        adminRole:true
-    };
-    console.log("load: ")
-    console.log(tokenPayload)
-    var Token = await jwt.sign(tokenPayload ,"thisistokensecret");
-    console.log("userToken: "+Token)
-    res.header("x-auth-token",Token)
-    // login success
-    res.send("Login success");
+async function postLogin(req, res) {
+  const user = req.body;
+
+  const validate = ajv.getSchema("userLogin");
+  const valid = validate(user);
+  // check the validation
+  if (!valid) return res.status(400).send(validate.errors);
+  // check the user exist
+  const userModel = new UserModel();
+  const userExist = await userModel.getUserByEmail(user.email);
+  if (!userExist) return res.status(401).send("failed to login");
+  // check the password
+  const isPasswordCorrect = await userModel.comparePassword(
+    user.password,
+    userExist.hash
+  );
+  if (!isPasswordCorrect) return res.status(401).send("failed to login");
+
+  tokenPayload = {
+    UserId: user.email,
+    adminRole: true,
+  };
+  console.log("load: ");
+  console.log(tokenPayload);
+  var Token = await jwt.sign(tokenPayload, "thisistokensecret");
+  console.log("userToken: " + Token);
+  res.header("x-auth-token", Token);
+  // login success
+  res.send("Login success");
 }
 
-async function postRegister(req,res){
-    const user = req.body;
-    console.log(user)
-    const validate = ajv.getSchema("userRegestraion");
-    const valid = validate(user);
-    
-    // check the validation
-    if(!valid) return res.status(400).send(validate.errors);
-    // check the user exist
-    const userModel = new UserModel();
-    const userExist = await userModel.getUserByEmail(user.email);
-    if(userExist) return res.status(400).send("Email already exist");
-    // register success
-    const newUser = await userModel.createUser(user);
-    res.send(newUser);
+async function postRegister(req, res) {
+  const user = req.body;
+  console.log(user);
+  const validate = ajv.getSchema("userRegestraion");
+  const valid = validate(user);
 
+  // check the validation
+  if (!valid) return res.status(400).send(validate.errors);
+  // check the user exist
+  const userModel = new UserModel();
+  const userExist = await userModel.getUserByEmail(user.email);
+  if (userExist) return res.status(400).send("Email already exist");
+  // register success
+  const newUser = await userModel.createUser(user);
+  res.send(newUser);
 }
 
-async function getAll(req,res){
-    const userModel = new UserModel();
-    const users = await userModel.getAllUsers();
-    res.send(users);
+async function getAll(req, res) {
+  const userModel = new UserModel();
+  const users = await userModel.getAllUsers();
+  res.send(users);
 }
 
-async function getProfile(req,res){
-    const userModel = new UserModel();
-    const user = await userModel.getUserByEmail(req.params.email);
-    res.send(user);
+async function getProfile(req, res) {
+  const userModel = new UserModel();
+  const user = await userModel.getUserByEmail(req.params.email);
+  res.send(user);
 }
 
 module.exports = {
-    postLogin,
-    postRegister,
-    getAll,
-    getProfile
-}
+  postLogin,
+  postRegister,
+  getAll,
+  getProfile,
+};
--- src/routes/Proudct.ts
+++ src/routes/Proudct.ts
@@ -1,8 +1,12 @@
-import express from "express";
+const express = require("express");
+const ProductController = require("../controllers/Product.Controller");
+
 const router = express.Router();
 
-router.get("/", function (req, res) {
-  res.send("product GET endpoint");
-});
+router.get("/", ProductController.GETProducts);
+router.get("/:id", ProductController.GETProductById);
+router.post("/", ProductController.POSTProduct);
+router.delete("/:id", ProductController.DELETEProductById);
+router.patch("/:id", ProductController.UPDATEProductById);
 
 module.exports = router;
