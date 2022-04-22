0733cc9503f4e991b780f542b7b6011ea17c6972
AhmSaleh <ahmedsaleh166@gmail.com>
2022-04-22 18:28:41 +0200

    configure Client router and add home, main-layout and not-found components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { MainLayoutComponent } from './Components/main-layout/main-layout.component';
+import { NotFoundComponentComponent } from './Components/not-found/not-found-component.component';
+import { RegisterComponent } from './Components/register/register.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', redirectTo: '/home', pathMatch: 'full' }, //Default path
+      { path: 'home', component: HomeComponent },
+      { path: 'register', component: RegisterComponent },
+    ],
+  },
+  { path: '**', component: NotFoundComponentComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
