b5394adc444d34b3e538e7135ca073c825918f62
abdo hashem <ar1813@fayoum.edu.eg>
2022-04-24 00:57:15 +0200

    add missing roles

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,7 +6,10 @@ import checkRole from "../middleware/authentication";
 import checkSchema from "../middleware/validation";
 import { RoleEnum } from "../model/UserModel";
 
-router.post("/",UserController.postLogin);
+router.post("/",
+    checkSchema("userLogin"),
+    UserController.postLogin);
+
 router.post("/register",checkSchema("userRegestraion"),UserController.postRegister);
 router.post("/register/admin",
             checkRole(RoleEnum.admin),
@@ -17,6 +20,6 @@ router.post("/register/admin",
 router.get("/all",checkRole(RoleEnum.admin),UserController.getAll);
 
 
-router.get("/:email",UserController.getProfile);
+router.get("/:email",checkRole(RoleEnum.admin,RoleEnum.merchent,RoleEnum.user),UserController.getProfile);
 
 export default router;
