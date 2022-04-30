64a1c8078764f3798a77e201733b615d2463db4c
abdo hashem <ar1813@fayoum.edu.eg>
2022-04-30 14:10:58 +0200

    remove unused schema

--- src/Utils/validate.ts
+++ src/Utils/validate.ts
@@ -4,7 +4,6 @@ const addFormats = require("ajv-formats");
 import regester_admin from "./SchemaRegester" 
 import regester_user from "./SchemaRegesterForUser"
 import login_user from "./SchemaLogin" 
-const post_post = require("./SchemaPost.json");
 import product_crud from "./Schema.Product"
 
 const ajv = (exports.ajv = new Ajv());
@@ -14,7 +13,6 @@ ajv.addSchema(regester_user, "userRegestraion");
 ajv.addSchema(regester_admin, "RegestraionByAdmin");
 ajv.addSchema(login_user, "userLogin");
 
-ajv.addSchema(post_post, "post");
 ajv.addSchema(product_crud, "product");
 
 export default ajv;
