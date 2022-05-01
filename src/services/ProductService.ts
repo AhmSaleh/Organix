6f4c78f8cc95376068ca3d0743940ea29cef97d7
abdo hashem <ar1813@fayoum.edu.eg>
2022-05-01 15:42:13 +0200

    Revert "Revert "Feat categoery""
    This reverts commit 09508d904dd12fb35c81abf48c91351f8db99814.

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,3 +1,4 @@
+import CategoryModel from "../model/Categoery";
 import { IProduct, ProductModel } from "../model/Product.Model";
 import CategoeryService from "./CategoeryService";
 
@@ -44,9 +45,14 @@ class ProductService {
     }
   }
 
-  async getProductByCategory(_category: string) {
+  async getProductByCategory_noRef(_category: string) {
     return await ProductModel.find({ categoryName: _category });
   }
+  
+  async getProductByCategory(categoryName: string) {
+    return await CategoryModel.findOne({ name: categoryName }).populate("products");
+  }
+
 }
 
 export default new ProductService();
