c388b468f76c4b463f16b77959b54749fd11d7a7
Mohamed <mohamedhatemibrahim@gmail.com>
2022-05-08 23:23:10 +0200

    more adjustments

--- src/Utils/Schema.Product.ts
+++ src/Utils/Schema.Product.ts
@@ -1,73 +1,74 @@
-import { JSONSchemaType } from "ajv"
+import { JSONSchemaType } from "ajv";
 import { IProduct } from "../model/Product.Model";
 
-const procut_schema: JSONSchemaType<IProduct & {merchantId:string}> = {
-    "type": "object",
-    "properties": {
-        "name": {
-            "type": "string"
-        },
-        "rate": {
-            "type": "number",
-            "minimum": 0,
-            "maximum": 5
-        },
-        "price": {
-            "type": "number",
-            "exclusiveMinimum": 0
-        },
-        "shortDescription": {
-            "type": "string",
-            "minimum": 0
-        },
-        "availability": {
-            "type": "boolean"
-        },
-        "weight": {
-            "type": "number",
-            "minimum": 0
-        },
-        "availableInventory": {
-            "type": "number",
-            "minimum": 0
-        },
-        "longDescription": {
-            "type": "string"
-        },
-        "productInformation": {
-            "type": "string"
-        },
-        "categoryName": {
-            "type": "string"
-        },
-        "imagesURL": {
-            "type": "array",
-            "items": {
-                "type": "string",
-                "format": "uri"
-            }
-        },
-        "imgURL": {
-            "type": "string",
-            "format": "uri"
-        },
-        "merchantId": {
-            "type": "string",
-            "pattern": "^[0-9a-fA-F]{24}$"
-        }
+const procut_schema: JSONSchemaType<IProduct & { merchantId: string }> = {
+  type: "object",
+  properties: {
+    name: {
+      type: "string",
     },
-    "required": [
-        "name",
-        "price",
-        "shortDescription",
-        "availability",
-        "weight",
-        "longDescription",
-        "productInformation",
-        "categoryName",
-        "merchantId"
-    ]
-}
-
+    rate: {
+      type: "number",
+      minimum: 0,
+      maximum: 5,
+    },
+    price: {
+      type: "number",
+      exclusiveMinimum: 0,
+    },
+    shortDescription: {
+      type: "string",
+      minLength: 0,
+      maxLength: 70,
+    },
+    availability: {
+      type: "boolean",
+    },
+    weight: {
+      type: "number",
+      exclusiveMinimum: 0,
+    },
+    availableInventory: {
+      type: "number",
+      minimum: 0,
+    },
+    longDescription: {
+      type: "string",
+      minLength: 0,
+    },
+    productInformation: {
+      type: "string",
+    },
+    categoryName: {
+      type: "string",
+    },
+    imagesURL: {
+      type: "array",
+      items: {
+        type: "string",
+        format: "uri",
+      },
+    },
+    imgURL: {
+      type: "string",
+      format: "uri",
+    },
+    merchantId: {
+      type: "string",
+      pattern: "^[0-9a-fA-F]{24}$",
+    },
+  },
+  required: [
+    "name",
+    "price",
+    "shortDescription",
+    "availableInventory",
+    "weight",
+    "longDescription",
+    "productInformation",
+    "categoryName",
+    "imgURL",
+  ],
+};
 
 export default procut_schema;
