a28b2fe983d9e6c48bbc8c637c63dbc7683d43ee
AhmSaleh <ahmedsaleh166@gmail.com>
2022-05-18 19:03:44 +0200

    add add-product-to-cart in product item componenet

--- client/src/app/Components/shop/product-item/product-item.component.ts
+++ client/src/app/Components/shop/product-item/product-item.component.ts
@@ -1,26 +1,30 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { IProduct } from 'src/app/Models/IProdcut';
-
+import { CartService } from 'src/app/Services/cart.service';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.css']
+  styleUrls: ['./product-item.component.css'],
 })
 export class ProductItemComponent implements OnInit {
-
-  constructor() { 
+  constructor(private cartService: CartService) {
     this.product = {} as IProduct;
   }
 
   ngOnInit(): void {
     this.currentImage = {
-      'background-image': `url(${this.product.imgURL})`
+      'background-image': `url(${this.product.imgURL})`,
     };
   }
 
   currentImage: Record<string, string> = {};
 
-
   @Input() product: IProduct;
+
+  addProductIntoCart() {
+    this.cartService.add(this.product);
+    Notify.success('Product added to cart Successfully!', { timeout: 1400 });
+  }
 }
